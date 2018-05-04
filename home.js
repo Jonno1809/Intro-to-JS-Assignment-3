7cead49eae13908438fed424e3224f58c74c740c
Jonathan Simmons <jonathan.p.simmons96@gmail.com>
2018-05-04 22:40:47 +1000

    Add scroll progress bar

--- home.js
+++ home.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
-    // $("body").css("background-color","red");
     advanceProgressBar(calculateScrollPercentage());
 
+    // Scroll event listener
     $(document).scroll(function () { 
         advanceProgressBar(calculateScrollPercentage());
     });
 });
 
+/**
+ * Advances the scroll progress bar based on the scroll percentage
+ * @param {number} percentage - Scroll percentage
+ */
 function advanceProgressBar(percentage) {
     $('#progress-indicator').css("width", percentage+"%");
 }
 
+/**
+ * Calculate the percentage of how much the page has been scrolled
+ */
 function calculateScrollPercentage() {
     var scrollPosition = $(document).scrollTop();
     var windowHeight = parseInt(document.body.scrollHeight) - parseInt(document.body.clientHeight);
@@ -19,5 +26,5 @@ function calculateScrollPercentage() {
         var percentage = (scrollPosition / windowHeight) * 100;
         return percentage;
     }
-    return 100;
+    return 100; // If can't find window height, just fill progress bar
 }
