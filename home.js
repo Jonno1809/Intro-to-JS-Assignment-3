1585f578f34ec7cff7d803ae6360e81a8060a4aa
Jonathan Simmons <jonathan.p.simmons96@gmail.com>
2018-05-13 13:42:17 +1000

    Add animations

--- home.js
+++ home.js
@@ -1,3 +1,12 @@
+/**
+ * @author Jonathan Simmons
+ * Student Id: u3141072
+ * As a software engineering student, this is definitely not my finest work.
+ * I'm also no good at making things look pretty.
+ * 
+ * NOTE: If not running on a http server, you must run this in firefox as chrome doesn't allow local json files to be opened
+ */
+
 var khGames = getKhJSONDetails(); // Array of all KH games
 var bundledGames = [] //Array of bundled kh games
 
@@ -6,24 +15,121 @@ $(document).ready(function () {
     sortGamesByReleaseOrder();
     renderAllGamesTimeline();
 
-    // Vue used for bundled games section
+    // Vue used for bundled games section to data bind
     var bundleVue = new Vue({
         el: "#bundle-order",
         data: {
             bundledGames
         }
     })
+    // To avoid calling jquery all the time
+    var khHeading = $('#KH-heading')
+    var intro = $('#intro')
     
     // Setup fullpage.js
     $('#fullpage').fullpage({
-        navigation: true,
+        navigation: true, // Dots on the side
         keyboardScrolling: true,
-        onLeave: function(index, nextIndex, direction) {
-            advanceProgressBar(calculateScrollPercentage(nextIndex));
+        
+        // Callback for when it leaves a section (scroll, basically)
+        // Adds and removes classes for animation.css
+        onLeave: function(index, nextIndex, direction) { // Section index starts at 1 (which is dumb)
+            advanceProgressBar(calculateScrollPercentage(nextIndex)); // Update progress bar
+
+            // References to jquery selectors (to avoid jQuery many calls)
+            //Info and image of game in upcoming section
+            var nextImage = $('#image'+nextIndex)
+            var nextInfo = $('#info'+nextIndex)
+
+            // Info and image of game in current section (the one we're leaving)
+            var currImage = $('#image'+index)
+            var currInfo = $('#info'+index)
+            
+            // Info and image of bundle games
+            var bundleImage = $('.bundle-image')
+            var bundleInfo = $('.bundle-info')
+
+            // Stats Heading element
+            var statsHeading = $('#stats-heading')
+
+            if (index == 1) {
+                khHeading.addClass('slideOutRight')
+                intro.addClass('slideOutLeft')
+            }
+            if (nextIndex == 1) {
+                khHeading.removeClass('fadeInDown slideOutRight')
+                intro.removeClass('fadeInUp slideOutLeft')
+                khHeading.addClass('slideInRight')
+                intro.addClass('slideInLeft')
+            }
+
+            // Use cross over animations every 2nd section just to spice things up a little
+            // Slide out elements on exit
+            if (index != 1) {
+                if (currImage.hasClass('.left')) { // If current image is on left and info on right
+                    currImage.removeClass('slideInRight')
+                    currInfo.removeClass('slideInLeft')
+                    currImage.addClass('slideOutLeft')
+                    currInfo.addClass('slideOutRight')
+                } else { // If current image is on right and info on left
+                    currImage.removeClass('slideInLeft')
+                    currInfo.removeClass('slideInRight')
+                    currImage.addClass('slideOutRight')
+                    currInfo.addClass('slideOutLeft')
+                }
+            }
+
+            //Slide in next elements
+            if (nextIndex != 1) {
+                if (nextImage.hasClass('.left')) { // If next image on left and info on right
+                    nextImage.removeClass('slideOutLeft')
+                    nextInfo.removeClass('slideOutRight')
+                    nextImage.addClass('slideInRight')
+                    nextInfo.addClass('slideInLeft')
+                } else { // If next image on right and info on left
+                    nextImage.removeClass('slideOutRight')
+                    nextInfo.removeClass('slideOutLeft')
+                    nextImage.addClass('slideInLeft')
+                    nextInfo.addClass('slideInRight')
+                }
+            }
+
+            // Exit animations for bundle section
+            if (index == 15) {
+                bundleImage.removeClass('zoomIn')
+                bundleInfo.removeClass('slideInLeft')
+                bundleImage.addClass('zoomOut')
+                bundleInfo.addClass('slideOutRight')
+            }
+
+            // Entry animations for bundle section
+            if (nextIndex == 15) { 
+                bundleImage.removeClass('zoomOut')
+                bundleInfo.removeClass('slideOutRight')
+                bundleImage.addClass('zoomIn')
+                bundleInfo.addClass('slideInLeft')
+            }
+
+            //Exit animations for stats section
+            if (index == 16) {
+                statsHeading.removeClass('fadeInUpBig')
+                statsHeading.addClass('fadeOutDownBig')
+            }
+
+            if (nextIndex == 16) {
+                statsHeading.removeClass('fadeOutDownBig')
+                statsHeading.addClass('fadeInUpBig')
+            }
+
+            console.log(statsHeading)
         },
-        afterRender: function() {
+        // Called once all sections are rendered
+        afterRender: function() { 
             advanceProgressBar(calculateScrollPercentage(1)); // Default scroll progress bar
-            createPieChart();
+            createPieChart(); // Pie chart at the bottom
+            //initial animations
+            khHeading.addClass("fadeInDown")
+            intro.addClass("fadeInUp")
         }
     });
 });
@@ -35,23 +141,8 @@ $(document).ready(function () {
 function advanceProgressBar(percentage) {
     $('#progress-indicator').animate({
         width: percentage + "%"
-    }, 700)
+    }, {duration: 700}) // 700 milliseconds, same amount of time as auto-scroll
 }
-
-/**
- * @deprecated due to not working for fullpage.js - use @function calculateScrollPercentage(sectionIndex) instead
- * Calculate the percentage of how much the page has been scrolled
- */
-// function calculateScrollPercentage() {
-//     var scrollPosition = $(document).scrollTop();
-//     var windowHeight = parseInt(document.body.scrollHeight) - parseInt(document.body.clientHeight);
-
-//     if (windowHeight || windowHeight > 0) {
-//         var percentage = (scrollPosition / windowHeight) * 100;
-//         return percentage;
-//     }
-//     return 100; // If can't find window height, just fill progress bar
-// }
 
 /**
  * Calculate the percentage of how much the page has been scrolled
@@ -78,6 +169,8 @@ function getKhJSONDetails() {
 }
 
 /**
+ * Didn't actually end up using this, but could be good in the future
+ * 
  * Sort the games by the order they should be played
  * @param {array} games the array of game-details objects
  */
@@ -87,6 +180,8 @@ function sortGamesByPlayOrder() {
 }
 
 /**
+ * Didn't actually end up using this, but could be good in the future
+ * 
  * Sort the games by chronological order they are set in the KH universe
  */
 function sortGamesByChronologicalOrder() {
@@ -130,40 +225,53 @@ function sortGamesByReleaseOrder() {
 
 /**
  * Creates a html section element for each game so that fullpage.js can reder it.
- * I wouldn't have to do this if Vue.js supported v-for in components that render other elements >:(
- * Until I realised theres an official fullpage.js vue component at the last minute...
+ * I wouldn't have to do this if Vue.js supported v-for in components that render other elements (in this case, the .section class)
+ * Until I realised theres an official fullpage.js vue component at the last minute, but ran out of time to learn how use it (:sadface:)
+ *  
+ * So this is definitely not the most eloquent solution and I'm aware of that
  */
 function renderAllGamesTimeline() {
-    var i = 0;
+    var index = 2; // Section number relative to the entire page (1 is the intro so we start with 2)
 
     khGames.forEach(game => {
         if (!game.games) { // Don't include the bundles - they'll come later
-            $('#bundle-order').before( // Using before instead of after so that it's in order
-                '<div class="section pad col-md-6" id="'+i+'">'+ 
-                '<div id="info"'+i+">"+
-                "<img class='col-md-5' src=images/" + game.image +" id=image"+i+">" +
-                "<p>Name: "+game.name+"</p>" +
-                "<p>Year released: "+game.year+"</p>" +
-                "<p>Platform: "+game.platform+"</p>" +
-                '</div></div>'
-            );
-        }
+            $('#bundle-order').before( // Using before instead of after so that it's in order (saves another sort)
+                '<div class="section" id="index'+index+'">'+
+                    '<div class="d-flex align-items-center" id="row'+index+'">' +
+                        '<div class="col-md-6 animated" id="image'+index+'">'+
+                            '<img class="img-fluid rounded" src="images/'+game.image+'">'+
+                        '</div>' +
+                        '<div class="col-md-6 animated" id="info'+index+'">'+
+                            '<h1 class="display-4">'+game.name+'</h1>'+
+                            '<h4>Year: '+game.year+'</h4>'+
+                            '<h4>Platform: '+game.platform+'</h4>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+            )
+        
 
-        var element = $('#info'+ i)
+            var element = $('#info'+ index)
 
-        if (game.score) {
-            element.append("<p>Metacritic score: "+game.score+"</p>")
-        }
-        if (!(i % 2 == 0)) {
-            $('#'+i).addClass("offset-md-6 text-right")
-        }
+            if (game.score) { // Not all games have scores, so best to check to avoid NaN
+                element.append("<h4>Metacritic score: "+game.score+"</h4>")
+            }
 
+            if(index % 2 == 0) { // Alternate sides every 2nd section
+                $('#row'+index).addClass('flex-row')
+                $('#info'+index).addClass('text-right right')
+                $('#image'+index).addClass('left')
+            } else {
+                $('#row'+index).addClass('flex-row-reverse left')
+                $('#image'+index).addClass('right')
+            }
+            
+            index++; // Only update index if the element is rendered (i.e. not bundled games)
+        }
         // If a game bundle, don't render - we'll use Vue.
         if (game.games && game.platform == 'PlayStation 4') {
             bundledGames.push(game);
         }
-
-        i++;
     });
 
 }
@@ -189,13 +297,14 @@ function getBundledGames() {
 function createPieChart() {
     var names = [];
     var sales = [];
-    var colours = []
+    var colours = [];
 
     // RGB for the pie chart sections
     var red = 0;
     var blue = 0;
     var green = 0;
 
+    // Loop over each game in khGames and check if there's a 'sold' value, then add the game name and sold value to arrays  
     khGames.forEach(game => {
         if (game.sold) {
             names.push(game.name)
@@ -210,21 +319,22 @@ function createPieChart() {
         }
     });
 
+    var canvas = $('#pie-chart') // Where the graph with be rendered
 
-    var canvas = $('#pie-chart')
+    // Create a new 
     var chart = new Chart(canvas, {
         type: 'pie',
         data: {
             labels: names,
             datasets: [{
-                data: sales,
-                backgroundColor: colours
+                data: sales, // Sales data
+                backgroundColor: colours //Colours for the sections
             }]
         },
         options: {
             legend: {
                 labels: {
-                    fontColor: '#FFF'
+                    fontColor: '#FFF' //Legend Label white
                 }
             }
         }
